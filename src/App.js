820ed955eaa9bf220daa54f0780b98c0584046f8
AlHanouf AlToaimi <aaltoaimi@stcs.com.sa>
2021-08-08 01:34:49 +0300

    V3

--- src/App.js
+++ src/App.js
@@ -4,85 +4,79 @@ import React, { Component } from 'react'
 import {connect} from 'react-redux';
 import reducer from './reducer';
 
-
 class App extends Component {  
   constructor(props){
     super(props);
-  this.state = { todoTitle: ""}
+    this.state = { todoTitle: "", nextId:4}
   }
   
   handleChange=(event)=> {
     this.setState({[event.target.name]:event.target.value});
-  }
-
-handleSubmit=(event)=> {
-  event.preventDefault();
-  //this.setState({[event.target.name]:event.target.value});
-  this.props.addTodo({ id: this.props.toDoList.length,task: this.state.todoTitle });
-  this.state = { todoTitle: ''};
-}
-
-
-render() { 
-
-return (
-
-<div className="container">
-  <h2 style={{color:"DodgerBlue"}}>Track Your Task</h2>
-     
-  <form onSubmit={this.handleSubmit} className="form-group" > 
-          <input name="todoTitle" type="text" className="form-control" placeholder="Enter New Task" onChange={this.handleChange}/>
-          <br/> <input className="btnn" type="submit" value="Add New Task" className="btn btn-success" />
-          {/* <input type="reset" value="reset" className="btn btn-secondary"/> */}
-        </form>
-
-        <br/> <h3>To Do List</h3>
-
-        <table border="2" className="table table-striped">
-    <thead>
-      <tr>
-        <th>Task ID</th>
-        <th>Task Name</th>
-        <th>Complete</th>
-        <th> </th>
-        </tr>
-        </thead>
-        <tbody>
-      {  this.props.toDoList.map(({ id, task }) =>(
-          <tr key={id}>
-      <td>{id}</td>
-      <td>{task}</td>
-     
-      <td><input  className="form-check-input" type="checkbox" value="Done"/> </td>
-      <td> <input type="button" value="Delete Task" className="btn btn-danger"
-      // onClick={() => this.handleDelete({id})}
- onClick={()=>this.props.deleteTodo({id,task })} /></td> 
-
-      </tr> ))}
-          </tbody>
-          </table>
-    </div> 
-          )
+  } 
   
-      }}
+  handleSubmit=(event)=> {
+    event.preventDefault();
+    this.props.addTodo({ id: this.state.nextId++,task: this.state.todoTitle });
+    this.state = { todoTitle: ''};
+  }
+  
+  render() { 
+
     
-
-      const mapStateToProps = (state)=> {
-        return{
-          toDoList:state.toDoList,
-          id:state.id,
-          task:state.task
-        }
-      }
-     
-      
-      const mapDispatchToProps = (dispatch)=> {
-        return{
-          addTodo:(todo)=>dispatch({type:"ADD",payload:{ todo }}),
-          deleteTodo:(todo)=>dispatch({type:"DELETE",payload:{ todo }})
-      
-        }
-      }
-
-
+      const mystyle = {
+       
+        backgroundColor: "palevioletred",
+        padding: "10px",
+        fontFamily: "Arial"
+      };
+    
+    return (
+    
+    <div className="container-fluid" style={mystyle}>
+      <h2 style={{color:"DodgerBlue"}}>Track Your Task</h2>
+      <form className="form-group" > 
+      <input name="todoTitle" type="text" className="form-control" placeholder="Enter New Task" onChange={this.handleChange}/>
+      <br/> <input className="btnn" type="submit" value="Add New Task" className="btn btn-success" onClick={this.handleSubmit}/>
+          <input type="reset" value="reset" className="btn btn-secondary"/> 
+          </form>
+          
+          <br/> <h3>To Do List</h3>
+          <table border="2" className="table table-striped">
+            <thead>
+              <tr>
+                <th>Task Description</th>
+                <th>Complete</th>
+                <th> </th>
+                </tr>
+                </thead>
+                <tbody>
+                  {this.props.toDoList.map(({id,task})=>(
+                  <tr key={id}>
+                    <td>{task}</td>
+                    
+                    <td><input  className="form-check-input" type="checkbox" value="Done"/> </td>
+                    <td><input type="button" value="Delete Task" className="btn btn-danger" onClick={()=>this.props.deleteTodo({id,task })} /></td> 
+                    </tr> ))}
+                    </tbody>
+                    </table>
+                    </div> 
+                    )
+                  }
+                }
+                
+const mapStateToProps = (state)=> {
+  return{
+    toDoList:state.toDoList,
+    id:state.id,
+    task:state.task
+  }
+}
+                
+const mapDispatchToProps = (dispatch)=> {
+  return{
+    addTodo:(todo)=>dispatch({type:"ADD",payload:{ todo }}),
+    deleteTodo:(todo)=>dispatch({type:"DELETE",payload:{ todo }})
+  }
+}
+                
 export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/reducer.js
+++ src/reducer.js
@@ -1,36 +1,32 @@
 const initialState = {
-    toDoList: [{ id: 0, task: 'Attend MERN Class' },
-    {id:1,task:"Send XY Email"},
-    {id:2,task:"Call School"}
+    toDoList: [
+        {id:1,task:"Attend MERN Class"},
+        {id:2,task:"Send XY Email"},
+        {id:3,task:"Call School"}
     ]
-  
-  };
-  
-  const reducer = (state = initialState, action) => {
-  console.log(state)
-  //let newState = {...state}; 
-  
-    switch (action.type) {
-      case 'ADD':
-        return { ...state, toDoList: [...state.toDoList, action.payload.todo] };
-  
-   // break;
-  
-     case 'DELETE':{
+};
 
+
+const reducer = (state = initialState, action) => {
     
-           
+    switch (action.type) {
+        
+        case 'ADD':
             return {
-              ...state,
-              toDoList: [...state.toDoList.filter((todo) => todo.id !== action.payload.todo.id)]
+                ...state, toDoList: [...state.toDoList, action.payload.todo] 
             };
-          }
-   
-      //       break;
-                
-  
-  default:
-  return state;}
-  };
-  
-  export default reducer;
+            // break;
+            
+        case 'DELETE':
+            return {
+                ...state,
+                toDoList: [...state.toDoList.filter((todo) => todo.id !== action.payload.todo.id)]
+            };
+            // break;
+            
+        default:
+            return state;
+        }
+    };
+
+export default reducer;
